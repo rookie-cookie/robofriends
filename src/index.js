a6e1f46ee5868b205b2ed558eabebb4f32e61ac6
Jona Reyes <jonamreyes@gmail.com>
2018-07-08 14:46:05 -0400

    creating new component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//react-native - for mobile devices
+//importing css - react allows css to be added in every component
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import App from './App';
+import Hello from './Hello';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+//registerServiceWorker();
