f4a8e1d980acc169d95a49582c00c7d836ca1fef
Jona Reyes <jonamreyes@gmail.com>
2018-07-16 20:40:11 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 //react-native - for mobile devices
 //importing css - react allows css to be added in every component
 import './index.css';
-//import App from './App';
-import Hello from './Hello';
-//import registerServiceWorker from './registerServiceWorker';
+import Card from './Card';
+import 'tachyons'
+import registerServiceWorker from './registerServiceWorker';
 
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
-//registerServiceWorker();
+//create Card component
+ReactDOM.render(<Card />, document.getElementById('root'));
+registerServiceWorker();
