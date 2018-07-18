a01ecb38c01a8645ac18c2019d040f6ca2b06b7f
Jona Reyes <jonamreyes@gmail.com>
2018-07-17 22:05:47 -0400

    clean up

---
+++ src/CardList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card';
+
+const Cardlist = ({robots}) => {
+// use javascipt map  when looping or returning multiple components 
+return (
+	<div>
+		{
+			robots.map((user,i) => {
+	//in react, remember to add a key prop, this will allow react to know if a component is deleted
+	//react will keep track and therefore not have to rerun the entire dom is something changes
+	return (
+	 <Card 
+		 key={i} 
+		 id={robots[i].id} 
+		 name={robots[i].name} 
+		 email={robots[i].email} 
+	 />
+	 )
+	})
+		}
+	</div>
+	);
+}
+
+export default Cardlist;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import ReactDOM from 'react-dom';
 //react-native - for mobile devices
 //importing css - react allows to add css in every component
 import './index.css';
-import Card from './Card.js';
+// import Card from './Card.js';
 import 'tachyons'
 import registerServiceWorker from './registerServiceWorker';
 import {robots} from './robots';
+import CardList from './CardList';
 
 //create Card component
 ReactDOM.render(
-	<div>
-		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-		<Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-	</div>
+	<CardList robots={robots} />
 , document.getElementById('root'));
 registerServiceWorker();
