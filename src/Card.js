e0ba52b8f78e2a957021312428bc89dc5376bda1
Jona Reyes <jonamreyes@gmail.com>
2018-07-17 21:30:38 -0400

    adding cards

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
+const {name, email, id } = props;
   return (
-    <div>
-      <img src='' alt='robots'/>
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${id}.png?200x200`} alt='robots'/>
       <div>
-        <h2>Jane Doe </h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 //react-native - for mobile devices
-//importing css - react allows css to be added in every component
+//importing css - react allows to add css in every component
 import './index.css';
-import Card from './Card';
+import Card from './Card.js';
 import 'tachyons'
 import registerServiceWorker from './registerServiceWorker';
+import {robots} from './robots';
 
 //create Card component
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+		<Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+	</div>
+, document.getElementById('root'));
 registerServiceWorker();
