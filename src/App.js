3d63318bb8d24ed639a28b74a26304a90edcb8a2
Jona Reyes <jonamreyes@gmail.com>
2018-07-19 20:36:19 -0400

    filtering search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
+// STATE >> props
+// state - an object that describes your application, things that can change and affect the app
+// it usually lives in the parent component
+// props - things that come out of state
 
-const App = () => {
-	return (
+
+class App extends Component{
+	constructor (){
+		super()
+		//app state - it is what describes the app
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchchange = (event) => {
+		//setState is builtin in react
+		this.setState({searchfield: event.target.value});	
+	}
+
+
+	render () {
+		const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
 		<div className = 'tc'>
 			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={robots} />
+			<SearchBox searchChange={this.onSearchchange}/>
+			<CardList robots={filteredRobots} />
 		</div>
-	);
+		)
+	}
 }
 
 export default App;
--- src/CardList.js
+++ src/CardList.js
@@ -7,18 +7,18 @@ return (
 	<div>
 		{
 			robots.map((user,i) => {
-	//in react, remember to add a key prop, this will allow react to know if a component is deleted
-	//react will keep track and therefore not have to rerun the entire dom is something changes
-	return (
-	 <Card 
-		 key={i} 
-		 id={robots[i].id} 
-		 name={robots[i].name} 
-		 email={robots[i].email} 
-	 />
-	 )
-	})
-		}
+			//in react, remember to add a key prop, this will allow react to know if a component is deleted
+			//react will keep track and therefore not have to rerun the entire dom is something changes
+			return (
+			 <Card 
+				 key={i} 
+				 id={robots[i].id} 
+				 name={robots[i].name} 
+				 email={robots[i].email} 
+			 />
+	 	)
+			})
+	}
 	</div>
 	);
 }
